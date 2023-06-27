5d60eedcf36301e548d03f404f52d7389ab3e33f
eyal-solomon-1 <eyal2561999@gmail.com>
2023-06-27 08:16:55 +0300

    UI offline mode

--- ui/src/App.js
+++ ui/src/App.js
@@ -6,6 +6,7 @@ import Video from "./pages/Video";
 import Missing from "./pages/Missing";
 import Unauthorized from "./pages/Unauthorized";
 import RequireAuth from "./pages/RequireAuth";
+import Out from "./pages/Out";
 import PersistLogin from "./pages/PersistLogin";
 import { Routes, Route } from "react-router-dom";
 import NewVideo from "./pages/NewVideo";
@@ -14,19 +15,20 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Out />} />
         {/* public routes */}
-        <Route path="login" element={<Login />} />
+        {/* <Route path="login" lement={<Login />} />
         <Route path="register" element={<Register />} />
-        <Route path="unauthorized" element={<Unauthorized />} />
+        <Route path="unauthorized" element={<Unauthorized />} /> */}
 
         {/* Protected routes */}
-        <Route element={<PersistLogin />}>
+        {/* <Route element={<PersistLogin />}>
           <Route element={<RequireAuth />}>
             <Route path="/" element={<Home />} />
             <Route path="videos" element={<Video />} />
             <Route path="new-video" element={<NewVideo />} />
           </Route>
-        </Route>
+        </Route> */}
 
         {/* catch all */}
         <Route path="*" element={<Missing />} />
---
+++ ui/src/pages/Out.js
@@ -0,0 +1,10 @@
+const Out = () => {
+  return (
+    <article>
+      <h1>Oops</h1>
+      <p>We are temporarily offline .. we will be back soon !</p>
+    </article>
+  );
+};
+
+export default Out;
